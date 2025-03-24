f35e6483dcf55e5d05c6d7156eb0a7121f259abc
Torch <20874779+sooluh@users.noreply.github.com>
2025-03-24 15:28:38 +0700

    fix: google login callback

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,8 +1,8 @@
 import { Google } from 'arctic';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK } from '$env/static/private';
 
 export const google = new Google(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-  'http://localhost:5173/login/google/callback'
+  GOOGLE_CALLBACK || 'http://localhost:5173/login/google/callback'
 );
